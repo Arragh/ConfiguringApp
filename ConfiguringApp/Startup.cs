3f0881c98e5445c41b092bbf51ea0648e06d4586
Arragh <arragh@mail.ru>
2020-12-31 16:49:14 +0300

    isDevelopment

--- ConfiguringApp/Startup.cs
+++ ConfiguringApp/Startup.cs
@@ -1,13 +1,8 @@
 using ConfiguringApp.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ConfiguringApp
 {
@@ -24,18 +19,18 @@ namespace ConfiguringApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseMiddleware<ErrorMiddleware>();
+                app.UseMiddleware<BrowserTypeMiddleware>();
+                app.UseMiddleware<ShortCircuitMiddleware>();
+                app.UseMiddleware<ContentMiddleware>();
             }
 
             app.UseRouting();
             app.UseStaticFiles();
-            app.UseMiddleware<ErrorMiddleware>();
-            app.UseMiddleware<BrowserTypeMiddleware>();
-            app.UseMiddleware<ShortCircuitMiddleware>();
-            app.UseMiddleware<ContentMiddleware>();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute("", "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(null, "{controller=Home}/{action=Index}/{id?}");
 
                 //endpoints.MapGet("/", async context =>
                 //{
